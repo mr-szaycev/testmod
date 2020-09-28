2fbb13afd01b718f7aa5c1ab15a58e9e6f62f34f
suzma1980 <mr.szaycev@gmail.com>
2020-09-28 22:17:49 +0300

    Emphasize our friendliness

--- hi.go
+++ hi.go
@@ -4,10 +4,10 @@ import "fmt"
 
 // Hi returns a friendly greeting
 func Hi(name string) string {
-   return fmt.Sprintf("Hi, %s", name)
+   return fmt.Sprintf("Hi!, %s", name)
 }
 
 // By returns a bed girl
 func ByBy(name string) string {
-   return fmt.Sprintf("By, #{name}")
+   return fmt.Sprintf("By fack !, #{name}")
 }
